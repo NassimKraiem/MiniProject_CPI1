a476849de24052c91b504f7a2a58958d13b0fcf0
NassimKraiem <kakik2003@gmail.com>
2023-03-20 22:59:58 +0100

    + tabs

--- aside.py
+++ aside.py
@@ -1,3 +1,4 @@
+from cmath import pi
 from PyQt5.QtWidgets import QPushButton
 
 
@@ -8,20 +9,24 @@ def resetNavigation():
     for reset in resets:
         reset(False)
 
-def flipState(w, state = None):
+def flipState(windows, w, state = None):
     w.setProperty('current', not w.property('current') if state == None else state)
     w.style().unpolish(w)
     w.style().polish(w)
+    if(state==True):
+        index = ((windows.top_aside.children().index(w)-1) // 2)
+        print(index)
+        windows.tabWidget.setCurrentIndex(index)
 
 def connectBtns(windows):
     for widget in windows.top_aside.children():
         if (isinstance(widget, QPushButton) and widget.property('current') != None):
-            widget.clicked.connect(lambda _, w=widget: (resetNavigation(), flipState(w)))
-            resets.append((lambda state, w=widget: flipState(w, state)))
+            widget.clicked.connect(lambda _, w=widget: (resetNavigation(), flipState(windows, w, True)))
+            resets.append((lambda state, w=widget: flipState(windows, w, state)))
 
 """
 top_aside = list(filter(lambda widget: isinstance(widget, QPushButton) and widget.property('current') != None, windows.top_aside.children()))
 for widget in top_aside:
-        widget.clicked.connect(lambda _, w=widget: (resetNavigation(), flipState(w)))
-        resets.append((lambda state, w=widget: flipState(w, state)))
+        widget.clicked.connect(lambda _, w=widget: (resetNavigation(), flipState(windows, w, True)))
+        resets.append((lambda state, w=widget: flipState(windows, w, state)))
 """
